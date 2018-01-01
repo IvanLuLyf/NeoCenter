b37f09ace91be2b0b092b447f8108c620f984ac5
IvanLuLyf <835498692@qq.com>
2018-01-02 01:50:57 +0800

    Commont Notify

--- app/controllers/FeedController.php
+++ app/controllers/FeedController.php
@@ -30,4 +30,63 @@ class FeedController extends Controller
         }
         $this->render();
     }
+
+    public function view($tid = 0, $page = 1)
+    {
+        if (isset($_GET['tid'])) $tid = $_GET['tid'];
+        if (isset($_GET['page'])) $page = $_GET['page'];
+        if ($this->_mode == 1) {
+            $api = $this->filter('Api', ['canFeed']);
+            if ($api['ret'] == 0) {
+                $this->assign('ret', 0);
+                $this->assign('status', 'ok');
+                $this->assign('page', $page);
+                if ($tid == 0) {
+                    $feeds = (new FeedModel())->listFeed($api['uid'], $page);
+                    $this->assign('feeds', $feeds);
+                } else {
+                    $feed = (new FeedModel())->getFeed($tid);
+                    $comments = (new CommentModel())->listComment(3, $tid, $page);
+                    $this->assign('feed', $feed);
+                    $this->assign('comments', $comments);
+                }
+            } else {
+                $this->assignAll($api);
+            }
+        }
+        $this->render();
+    }
+
+    public function comment($tid = 0)
+    {
+        if (isset($_GET['tid'])) $tid = $_GET['tid'];
+        if ($this->_mode == 1) {
+            if (isset($_POST['message'])) {
+
+                $api = $this->filter('Api', ['canFeed']);
+                if ($api['ret'] == 0) {
+                    if ($feed = (new FeedModel())->getFeed($tid)) {
+                        $message = $_POST['message'];
+                        $user = (new UserModel())->getUserByUid($api['uid']);
+                        $commentid = (new CommentModel())->sendComment(3, $tid, $user['username'], $user['nickname'], $message, time());
+                        (new NotificationModel())->notify(3, $tid, $feed['uid'], $api['uid'], 'comment', $message, time());
+                        $this->assign('ret', 0);
+                        $this->assign('status', 'ok');
+                        $this->assign('tid', $tid);
+                        $this->assign('cmid', $commentid);
+                    } else {
+                        $this->assign('ret', 3001);
+                        $this->assign('status', 'invalid tid');
+                    }
+                } else {
+                    $this->assignAll($api);
+                }
+
+            } else {
+                $this->assign('ret', 1004);
+                $this->assign('status', 'empty arguments');
+            }
+        }
+        $this->render();
+    }
 }
