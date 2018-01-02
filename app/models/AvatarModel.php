fbe4948a146e4e837183e3c5e5814c20f90e6706
IvanLuLyf <835498692@qq.com>
2018-01-02 11:19:41 +0800

    Avatar Model

---
+++ app/models/AvatarModel.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: IvanLu
+ * Date: 2018/1/2
+ * Time: 11:05
+ */
+class AvatarModel extends Model
+{
+    protected $table = 'tp_avatar';
+
+    public function upload($uid, $path)
+    {
+        if ($this->where(["uid = ?"], [$uid])->fetch()) {
+            $updates = array('url' => $path);
+            return $this->where(["uid = ?"], [$uid])->update($updates);
+        } else {
+            $datas = array('uid' => $uid, 'url' => $path);
+            return $this->add($datas);
+        }
+    }
+
+    public function getAvatar($uid)
+    {
+        if ($row = $this->where(["uid = ?"], [$uid])->fetch()) {
+            return $row['url'];
+        } else {
+            return '/static/images/avatar.jpg';
+        }
+    }
+}
