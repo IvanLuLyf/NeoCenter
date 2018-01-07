cd5c9eb1d8e88d02c3852f3ef9ad2720d5be749e
IvanLuLyf <835498692@qq.com>
2018-01-07 15:51:50 +0800

    Add Feed Image and Like Function

--- app/controllers/FeedController.php
+++ app/controllers/FeedController.php
@@ -16,7 +16,30 @@ class FeedController extends Controller
                 if ($api['ret'] == 0) {
                     $message = $_POST['message'];
                     $user = (new UserModel())->getUserByUid($api['uid']);
-                    $feedid = (new FeedModel())->sendFeed($message, $api['name'], $api['uid'], $user['username'], $user['nickname'], time());
+                    $imageCount = 0;
+                    if (isset($_FILES['images'])) {
+                        $paths = $_FILES['images']["tmp_name"];
+                        if (is_array($paths)) {
+                            $imageCount = count($paths);
+                        } else {
+                            $imageCount = 1;
+                        }
+                    }
+                    $feedid = (new FeedModel())->sendFeed($message, $api['name'], $api['uid'], $user['username'], $user['nickname'], time(), $imageCount);
+                    $attachModel = new AttachModel();
+                    if ($imageCount > 0) {
+                        for ($i = 0; $i < $imageCount; $i++) {
+                            if ((($_FILES["images"]["type"][$i] == "image/gif") || ($_FILES["images"]["type"][$i] == "image/jpeg") || ($_FILES["images"]["type"][$i] == "image/pjpeg"))
+                                && ($_FILES["images"]["size"][$i] < 2000000)
+                            ) {
+                                $t = time() % 1000;
+                                $filename = "feed/$feedid-$i-$t.jpg";
+                                $this->storage()->upload($filename, $_FILES["images"]["tmp_name"][$i]);
+                                $url = $this->storage()->geturl($filename);
+                                $attachModel->upload($api['uid'], $feedid, $url);
+                            }
+                        }
+                    }
                     $this->assign('ret', 0);
                     $this->assign('status', 'ok');
                     $this->assign('tid', $feedid);
@@ -43,9 +66,17 @@ class FeedController extends Controller
                 $this->assign('page', $page);
                 if ($tid == 0) {
                     $feeds = (new FeedModel())->listFeed($api['uid'], $page);
+                    foreach ($feeds as &$feed) {
+                        if ($feed['image'] != null && $feed['image'] > 0) {
+                            $feed['images'] = (new AttachModel())->getAttachByTid($feed['tid']);
+                        }
+                    }
                     $this->assign('feeds', $feeds);
                 } else {
                     $feed = (new FeedModel())->getFeed($tid);
+                    if ($feed['image'] != null && $feed['image'] > 0) {
+                        $feed['images'] = (new AttachModel())->getAttachByTid($feed['tid']);
+                    }
                     $comments = (new CommentModel())->listComment(3, $tid, $page);
                     $this->assign('feed', $feed);
                     $this->assign('comments', $comments);
@@ -85,6 +116,36 @@ class FeedController extends Controller
             } else {
                 $this->assign('ret', 1004);
                 $this->assign('status', 'empty arguments');
+            }
+        }
+        $this->render();
+    }
+
+    function ac_like()
+    {
+        if (isset($_REQUEST['tid'])) $tid = $_REQUEST['tid'];
+        if ($this->_mode == 1) {
+            $api = $this->filter('Api', ['canFeed']);
+            if ($api['ret'] == 0) {
+                $feedModel = new FeedModel();
+                if ($feed = $feedModel->getFeed($tid)) {
+                    if ((new LikeModel())->isLike($api['uid'], 3, $tid) == 1) {
+                        $this->assign('ret', 3002);
+                        $this->assign('status', 'already liked');
+                    } else {
+                        (new LikeModel())->like($api['uid'], 3, $tid);
+                        $like_num = $feedModel->likeFeed($tid);
+                        (new NotificationModel())->notify(3, $tid, $feed['uid'], $api['uid'], 'like', '', time());
+                        $this->assign('ret', 0);
+                        $this->assign('status', 'ok');
+                        $this->assign('like_num', $like_num);
+                    }
+                } else {
+                    $this->assign('ret', 3001);
+                    $this->assign('status', 'invalid tid');
+                }
+            } else {
+                $this->assignAll($api);
             }
         }
         $this->render();
